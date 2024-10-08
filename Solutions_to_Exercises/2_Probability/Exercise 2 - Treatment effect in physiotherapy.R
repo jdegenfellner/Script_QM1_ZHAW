b449e1cdd02bcc6a0c48bbea667181d3326cad14
jdegenfellner <jdegenfellner@gmail.com>
2024-10-08 16:56:25 +0200

    up

--- Solutions_to_Exercises/2_Probability/Exercise 2 - Treatment effect in physiotherapy.R
+++ Solutions_to_Exercises/2_Probability/Exercise 2 - Treatment effect in physiotherapy.R
@@ -3,17 +3,16 @@
 # For simplicity, let's just test two theta values: 0.3 (as in the previous study) and 0.4
 
 # Prior probability for theta = 0.3
-p_theta1 <- 0.85 # most weight is on the previous study, maybe because it was well executed and had a large sample size.
-# This part could be argued to be subjective. 
+p_theta1 <- 0.5 # 50% chance of theta = 0.3
 
 # Prior probability for theta = 20/50 = 0.4
-p_theta2 <- 0.25
+p_theta2 <- 0.5 # 50% chance of theta = 0.4
 
 # Likelihood of the data given theta = 0.3
-p_data_theta1 <- dbinom(20, 50, 0.3)
+p_data_theta1 <- dbinom(22, 50, 0.3)
 
 # Likelihood of the data given theta = 20/50 = 0.4
-p_data_theta2 <- dbinom(20, 50, 20/50)
+p_data_theta2 <- dbinom(22, 50, 20/50)
 
 # Marginal likelihood p(data), as a weighted sum of likelihoods
 p_data <- p_data_theta1 * p_theta1 + p_data_theta2 * p_theta2
