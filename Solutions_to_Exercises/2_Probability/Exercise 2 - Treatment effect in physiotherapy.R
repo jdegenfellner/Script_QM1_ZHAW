3480a6c2b7f693983db48154b4bea52d0c113918
jdegenfellner <jdegenfellner@gmail.com>
2024-10-29 09:50:21 +0100

    up

--- Solutions_to_Exercises/2_Probability/Exercise 2 - Treatment effect in physiotherapy.R
+++ Solutions_to_Exercises/2_Probability/Exercise 2 - Treatment effect in physiotherapy.R
@@ -9,10 +9,10 @@ p_theta1 <- 0.5 # 50% chance of theta = 0.3
 p_theta2 <- 0.5 # 50% chance of theta = 0.4
 
 # Likelihood of the data given theta = 0.3
-p_data_theta1 <- dbinom(22, 50, 0.3)
+p_data_theta1 <- dbinom(22, 50, 0.3) # theta1 = 0.3
 
 # Likelihood of the data given theta = 20/50 = 0.4
-p_data_theta2 <- dbinom(22, 50, 20/50)
+p_data_theta2 <- dbinom(22, 50, 0.4) # theta2 = 0.4
 
 # Marginal likelihood p(data), as a weighted sum of likelihoods
 p_data_theta_marginal <- p_data_theta1 * p_theta1 + p_data_theta2 * p_theta2
@@ -21,6 +21,12 @@ p_data_theta_marginal <- p_data_theta1 * p_theta1 + p_data_theta2 * p_theta2
 p_theta1_data <- p_data_theta1 * p_theta1 / p_data_theta_marginal
 p_theta2_data <- p_data_theta2 * p_theta2 / p_data_theta_marginal
 
-p_theta1_data
-p_theta2_data
+p_theta1_data # 0.1178669 # Probability that theta = 0.3 after seeing the new data
+p_theta2_data # 0.8821331 # Probability that theta = 0.4 after seeing the new data
 
+#Given a model selection problem in which one wishes to choose between 
+# two models on the basis of observed data D, the plausibility of the two 
+# different models M1 and M2, parametrised by model parameter vectors theta1 and 
+# theta2, is assessed by the Bayes factor K given by
+# Bayes factor: p(data|theta1) / p(data|theta2) = 
+p_data_theta1/p_data_theta2
